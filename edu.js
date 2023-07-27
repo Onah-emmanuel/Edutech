30b682451b0ce6d5b338d6a9f5a4f46e52f1ab6a
Onah Emmanuel Uchenna <onahemmanueluchenna@gmail.com>
2023-07-27 20:06:08 +0100

    changed some file locations

---
+++ edu.js
@@ -0,0 +1,18 @@
+// let welcome = document.querySelector(".welcomeback");
+// var mybackup = document.querySelector(".backdrop")
+// function dosomething() {
+    // hamburgerdiv.classList.toggle("showburger")
+    // mobileLinks.classList.toggle("show-mobile-links-holder")
+    // welcome.classList.toggle("welcomeback")
+    // window.location.href = document.getElementsByClassName("welcomeback")
+// }
+// var welcome = document.querySelector(".welcomeback")
+// document.getElementById("dosomething").onclick = function(){
+//     welcome.classList.toggle("show-welcomeback")
+// }
+var yam = document.querySelector('.hamburger')
+var link = document.querySelector('.mobile-links-holder')
+function Addclass() {
+    yam.classList.toggle("show")
+    link.classList.toggle("showlinks")
+}
